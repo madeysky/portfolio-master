6b36c747684833470392406a4b0518d5a4c8db8f
madeysky <pawelrmadej@gmail.com>
2022-06-09 20:49:28 +0200

    drugie podejscie

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Nawka from "./komponenty/nawka/Nawka";
+
+function App() {
+  return (
+    <Nawka/>
+  );
+}
+
+export default App;
