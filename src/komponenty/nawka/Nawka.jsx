5e296fd75a52f5c2bedf8a37458859dbf0dbc9a6
madeysky <pawelrmadej@gmail.com>
2022-06-09 20:58:13 +0200

    logo

--- src/komponenty/nawka/Nawka.jsx
+++ src/komponenty/nawka/Nawka.jsx
@@ -1,17 +1,17 @@
-import logo from "../../../src/logo.png";
+import logo from "../../../src/logo1.jpeg";
 import "./nawka.css";
 
 
 export default function Nawka() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+      <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
