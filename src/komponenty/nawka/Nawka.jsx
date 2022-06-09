23d5e284f221f2feeedb569c0bf670e7db6d43fb
madeysky <pawelrmadej@gmail.com>
2022-06-09 22:04:43 +0200

    podswietlanie

--- src/komponenty/nawka/Nawka.jsx
+++ src/komponenty/nawka/Nawka.jsx
@@ -5,7 +5,7 @@ import "./nawka.css";
 export default function Nawka() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <div className="container-fluid">
+  <div className="container">
     <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -13,13 +13,22 @@ export default function Nawka() {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">O mnie</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Services</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Doświadczenie</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Kontakt</a>
         </li>
         
       </ul>
