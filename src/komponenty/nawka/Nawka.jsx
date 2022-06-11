29e73ae5ce89b2a97af50e60443889fc9d42ee0d
madeysky <pawelrmadej@gmail.com>
2022-06-11 09:42:03 +0200

    ikonka hamburga

--- src/komponenty/nawka/Nawka.jsx
+++ src/komponenty/nawka/Nawka.jsx
@@ -1,6 +1,8 @@
 import logo from "../../../src/logo1.jpeg";
 import "./nawka.css";
-
+//React Fontawesome Imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Nawka() {
     return (
@@ -8,7 +10,7 @@ export default function Nawka() {
   <div className="container">
     <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <FontAwesomeIcon icon= {faBars} style={{color: "var(--primary-white)"}}/>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
